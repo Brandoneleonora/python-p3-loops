3838909a526ad77cb5c102e6040b525230098e8e
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-07-09 21:43:57 -0400

    complete project

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [i * i for i in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    def fizz(num):
+        if num % 3 == 0 and num % 5 == 0:
+            return 'FizzBuzz'
+        elif num % 3 == 0:
+            return 'Fizz'
+        elif num % 5 == 0:
+            return 'Buzz'
+        else:
+            return num
+
+
+    for i in range(100):
+        print(fizz(i))
